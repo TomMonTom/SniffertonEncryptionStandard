08656818a70b7b9c58c4a04a236dfd9879ee6d3b
TomMonTom <TomMonTom@users.noreply.github.com>
2020-06-22 17:29:16 -0500

    Update Extensions.swift

--- Extensions.swift
+++ Extensions.swift
@@ -43,3 +43,8 @@ func s(string:String) -> Data{
         let salt = string.sha256(salt: q.clean)
         return salt
 }
+extension Double {
+    var clean: String {
+        return self.truncatingRemainder(dividingBy: 1) == 0 ? String(format: "%.0f", self) : String(self)
+    }
+}
